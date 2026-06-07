c90b09431759195ac99f65e1b801f64600eee13d
aqilahalnawiswary <aqilahummu05@gmail.com>
2026-06-07 10:34:13 +0700

    add homepage phpunit test

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+    public function test_homepage_can_be_accessed(): void
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+}
