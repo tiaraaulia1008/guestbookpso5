fc7564e9c34628ae31ca4ece74875ca5d40bedc0
tiaraaulia <5026231148@student.its.ac.id>
2026-06-07 12:06:41 +0700

    coverage4

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -8,7 +8,7 @@ class HomePageTest extends TestCase
 {
     public function test_homepage_can_be_accessed(): void
     {
-        $response = $this->get('/');
+        $response = $this->get(route('home.index'));
         $response->assertStatus(200);
     }
 }
